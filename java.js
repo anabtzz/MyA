cea438ecd3bd31cc114bc4d9e791527b3cffb015
anabtzz <anaby65@gmail.com>
2024-11-05 03:02:58 -0300

    MyA

---
+++ java.js
@@ -0,0 +1,13 @@
+window.revelar = ScrollReveal({reset: true});
+
+revelar.reveal('.topo', 
+    {
+        duration: 2000,
+        distance: '20px',
+        delay: 1000
+    });
+    revelar.reveal('.titulo', 
+        {
+            duration: 2000,
+            distance: '80px'
+        });
